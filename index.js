6e8474146586c37e520ca7c7010b4f9df2d0b0fd
Dnica98 <daniela.nica98@gmail.com>
2024-10-30 20:09:06 +0200

    1

---
+++ index.js
@@ -0,0 +1,14 @@
+// const obj = {
+//     name: 'Alex',
+// }
+
+// console.log(Object.getOwnPropertyDescriptor(obj,'name'))
+
+// Object.defineProperty(obj, 'job', {
+//     value: 'dev',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true
+// })
+
+// console.log()
